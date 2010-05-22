d919a12d82042d5f17a2f4beac5485331edd5db8
themeshaper <themeshaper@d957f892-c61d-0410-b221-f235e6eecf30>
2010-05-22 23:17:22 +0000

    remove author bio from single.php
    git-svn-id: http://svn.automattic.com/wpcom-themes/toolbox@3679 d957f892-c61d-0410-b221-f235e6eecf30

--- single.php
+++ single.php
@@ -32,21 +32,6 @@
 						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'theme' ), 'after' => '</div>' ) ); ?>
 					</div><!-- .entry-content -->
 
-<?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
-					<div id="entry-author-info">
-						<div id="author-avatar">
-							<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'toolbox_author_bio_avatar_size', 60 ) ); ?>
-						</div><!-- #author-avatar -->
-						<div id="author-description">
-							<h2><?php _e( 'About ', 'theme' ); ?><?php the_author(); ?></h2>
-							<?php the_author_meta( 'description' ); ?>
-							<div id="author-link">
-								<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="<?php printf( esc_attr__( 'View all posts by %s', 'theme' ), get_the_author() ); ?>"><?php _e( 'View all posts by ', 'theme' ); ?><?php the_author(); ?> &rarr;</a>
-							</div><!-- #author-link	-->
-						</div><!-- #author-description -->
-					</div><!-- .entry-author-info -->
-<?php endif; ?>
-
 					<div class="entry-utility">
 					<?php
 						$tag_list = get_the_tag_list('', ', ');
