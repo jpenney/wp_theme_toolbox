50aa0a1157ef93796f49ab6e581f3e6d02bb55ab
themeshaper <themeshaper@d957f892-c61d-0410-b221-f235e6eecf30>
2010-05-30 03:55:56 +0000

    Toolbox: use a nested heading for the comments and add a nice message to the stylesheet
    git-svn-id: http://svn.automattic.com/wpcom-themes/toolbox@3771 d957f892-c61d-0410-b221-f235e6eecf30

--- comments.php
+++ comments.php
@@ -19,12 +19,12 @@
 ?>
 
 <?php if ( have_comments() ) : ?>
-			<h3 id="comments-title">
+			<h2 id="comments-title">
 <?php
     printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'theme' ),
         number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' );
 ?>
-            </h3>
+            </h2>
 
 <?php if ( get_comment_pages_count() > 1 ) : // are there comments to navigate through ?>
 			<div class="navigation">
--- footer.php
+++ footer.php
@@ -8,12 +8,8 @@
 	</div><!-- #main -->
 
 	<footer id="colophon">
-			<div id="site-info">
-				<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-			</div>
-
 			<div id="site-generator">
-				<a href="http://wordpress.org/" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'theme' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s.', 'theme' ), 'WordPress' ); ?></a>
+				<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'theme' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s.', 'theme' ), 'WordPress' ); ?></a>
 			</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
