7bd664091d73a71ad461bd367b4ceb0ae0e8bb3b
themeshaper <themeshaper@d957f892-c61d-0410-b221-f235e6eecf30>
2010-06-13 17:36:12 +0000

    Toolbox: better image attachment gallery linking
    git-svn-id: http://svn.automattic.com/wpcom-themes/toolbox@3968 d957f892-c61d-0410-b221-f235e6eecf30

--- image.php
+++ image.php
@@ -43,20 +43,28 @@
 						<div class="entry-attachment">
 							<div class="attachment">
 <?php
-	// Get the URL of the next image in a gallery for image attachment pages
+	/**
+	 * Grab the IDs of all the image attachments in a gallery so we can get the URL of the next adjacent image in a gallery,
+	 * or the first image (if we're looking at the last image in a gallery), or, in a gallery of one, just the link to that image file
+	 */
 	$attachments = array_values( get_children( array( 'post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID' ) ) );
-
 	foreach ( $attachments as $k => $attachment ) {
 		if ( $attachment->ID == $post->ID )
 			break;
 	}
-
 	$k++;
-
-	if ( isset( $attachments[ $k ] ) )
-		$next_attachment_url = get_attachment_link( $attachments[ $k ]->ID );
-	else
-		$next_attachment_url = get_permalink( $post->post_parent );
+	// If there is more than 1 attachment in a gallery
+	if ( count( $attachments ) > 1 ) {
+		if ( isset( $attachments[ $k ] ) )
+			// get the URL of the next image attachment
+			$next_attachment_url = get_attachment_link( $attachments[ $k ]->ID );
+		else
+			// or get the URL of the first image attachment
+			$next_attachment_url = get_attachment_link( $attachments[ 0 ]->ID );
+	} else {
+		// or, if there's only 1 image, get the URL of the image
+		$next_attachment_url = wp_get_attachment_url();
+	}
 ?>
 								<a href="<?php echo $next_attachment_url; ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment"><?php
 								$attachment_size = apply_filters( 'theme_attachment_size',  800 );
